eed5bf482ad41d95c4e6f49ab293cf2172339e30
Ole Troan <ot@cisco.com>
2018-08-20 19:17:20 +0200

    MAP: 64-bit DMR support.
    Signed-off-by: Ole Troan <ot@cisco.com>

--- src/plugins/map/ip6_map.c
+++ src/plugins/map/ip6_map.c
@@ -174,7 +174,7 @@ ip6_map (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
     vlib_node_get_runtime (vm, ip6_map_node.index);
   map_main_t *mm = &map_main;
   vlib_combined_counter_main_t *cm = mm->domain_counters;
-  u32 thread_index = vlib_get_thread_index ();
+  u32 thread_index = vm->thread_index;
 
   from = vlib_frame_vector_args (frame);
   n_left_from = frame->n_vectors;
@@ -244,8 +244,7 @@ ip6_map (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
 	       && clib_net_to_host_u16 (ip60->payload_length) > 20))
 	    {
 	      d0 =
-		ip6_map_get_domain (vnet_buffer (p0)->ip.adj_index[VLIB_TX],
-				    (ip4_address_t *) & ip40->
+		ip4_map_get_domain ((ip4_address_t *) & ip40->
 				    src_address.as_u32, &map_domain_index0,
 				    &error0);
 	    }
@@ -272,8 +271,7 @@ ip6_map (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
 	       && clib_net_to_host_u16 (ip61->payload_length) > 20))
 	    {
 	      d1 =
-		ip6_map_get_domain (vnet_buffer (p1)->ip.adj_index[VLIB_TX],
-				    (ip4_address_t *) & ip41->
+		ip4_map_get_domain ((ip4_address_t *) & ip41->
 				    src_address.as_u32, &map_domain_index1,
 				    &error1);
 	    }
@@ -457,8 +455,7 @@ ip6_map (vlib_main_t * vm, vlib_node_runtime_t * node, vlib_frame_t * frame)
 	       && clib_net_to_host_u16 (ip60->payload_length) > 20))
 	    {
 	      d0 =
-		ip6_map_get_domain (vnet_buffer (p0)->ip.adj_index[VLIB_TX],
-				    (ip4_address_t *) & ip40->
+		ip4_map_get_domain ((ip4_address_t *) & ip40->
 				    src_address.as_u32, &map_domain_index0,
 				    &error0);
 	    }
@@ -814,7 +811,7 @@ ip6_map_ip6_reass (vlib_main_t * vm,
 }
 
 /*
- * ip6_ip4_virt_reass
+ * ip6_map_ip4_reass
  */
 static uword
 ip6_map_ip4_reass (vlib_main_t * vm,
@@ -825,7 +822,7 @@ ip6_map_ip4_reass (vlib_main_t * vm,
     vlib_node_get_runtime (vm, ip6_map_ip4_reass_node.index);
   map_main_t *mm = &map_main;
   vlib_combined_counter_main_t *cm = mm->domain_counters;
-  u32 thread_index = vlib_get_thread_index ();
+  u32 thread_index = vm->thread_index;
   u32 *fragments_to_drop = NULL;
   u32 *fragments_to_loopback = NULL;
 
@@ -861,8 +858,7 @@ ip6_map_ip4_reass (vlib_main_t * vm,
 	  ip60 = ((ip6_header_t *) ip40) - 1;
 
 	  d0 =
-	    ip6_map_get_domain (vnet_buffer (p0)->ip.adj_index[VLIB_TX],
-				(ip4_address_t *) & ip40->src_address.as_u32,
+	    ip4_map_get_domain ((ip4_address_t *) & ip40->src_address.as_u32,
 				&map_domain_index0, &error0);
 
 	  map_ip4_reass_lock ();
@@ -1018,7 +1014,7 @@ ip6_map_icmp_relay (vlib_main_t * vm,
   vlib_node_runtime_t *error_node =
     vlib_node_get_runtime (vm, ip6_map_icmp_relay_node.index);
   map_main_t *mm = &map_main;
-  u32 thread_index = vlib_get_thread_index ();
+  u32 thread_index = vm->thread_index;
   u16 *fragment_ids, *fid;
 
   from = vlib_frame_vector_args (frame);
@@ -1177,6 +1173,13 @@ static char *map_error_strings[] = {
 };
 
 /* *INDENT-OFF* */
+VNET_FEATURE_INIT (ip6_map_feature, static) =
+{
+  .arc_name = "ip6-unicast",
+  .node_name = "ip6-map",
+  .runs_before = VNET_FEATURES ("ip6-flow-classify"),
+};
+
 VLIB_REGISTER_NODE(ip6_map_node) = {
   .function = ip6_map,
   .name = "ip6-map",
