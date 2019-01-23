d256ff10d7a488454075510e87655830b027fd1e
Ole Troan <ot@cisco.com>
2019-01-23 11:47:15 +0100

    hanat: First sketch of worker - mapper protocol
    Signed-off-by: Ole Troan <ot@cisco.com>

---
+++ src/plugins/hanat/protocol/hanat_protocol.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2019 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef included_hanat_protocol_h
+#define included_hanat_protocol_h
+
+/*
+ * Worker - Mapper protocol.
+ *
+ * A worker missing a binding for a session, it sends a
+ * SESSION_REQUEST to the mapper. The mapper responds with a
+ * SESSION_REPLY containing the NAT instructions, and which addresses
+ * / ports to translate to. The worker can pass in opaque data like
+ * e.g. a tunnel end-point address in case of DS-lite style tunneling.
+ *
+ * The worker will periodically send SESSION_UPDATE messages,
+ * indicating that the session is still active.  On detection of a
+ * session completing, e.g. a TCP FIN flag, the worker will send a
+ * SESSION_UPDATE message with the session_completing flag
+ * set. Likewise the mapper can send this message to the workers to
+ * indicate that it can clean up a session. A session will not be
+ * cleaned up immediately, but flagged for reuse.
+ *
+ */
+
+/*
+ * Message header
+ */
+typedef struct {
+  u32 core_id;
+} hanat_header_t;
+
+/*
+ * TLV Types
+ */
+#define HANAT_SESSION_REQUEST 0x00
+#define HANAT_SESSION_REPLY   0x01
+#define HANAT_SESSION_UPDATE  0x02
+
+/*
+ * Session descriptor
+ */
+typedef struct {
+  ip4_address_t sa;
+  ip4_address_t da;
+  u32 proto:8, vni:24;
+  u16 sp;
+  u16 dp;
+} hanat_session_descriptor_t;
+
+/*
+ * Session request / reply
+ */
+typedef struct {
+  u8 type;		/* Session request */
+  u8 length;
+  u32 session_id;
+  hanat_session_descriptor_t desc;
+  u8 opaque_data[0];
+} hanat_option_session_request_t;
+
+typedef struct {
+  u8 type;		/* Session reply */
+  u8 length;
+  u32 session_id;
+  hanat_instructions_t instructions;
+  /* Translated data fields */
+  ip4_address_t sa;
+  ip4_address_t da;
+  u16 sp;
+  u16 dp;
+  u8 opaque_data[0];
+} hanat_option_session_reply_t;
+
+/*
+ * Session update
+ */
+typedef enum {
+  HANAT_FLAGS_UPDATE     = 0x0,	      
+  HANAT_FLAGS_COMPLETING = 0x1,
+} hanat_flags_t;  
+
+typedef struct {
+  u8 type;		/* Session keepalive */
+  u8 length;
+  hanat_session_descriptor_t desc;
+  hanat_flags_t flags;
+  u64 packets;
+  u64 bytes;
+} hanat_option_session_update_t;
+
+#endif
